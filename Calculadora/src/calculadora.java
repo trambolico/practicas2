d69ec6922fe54e961bcdc6d66262717e887dfd43
DapinvaX <dapinva95@gmail.com>
2016-04-07 12:04:17 +0200

    Clase calculadora

---
+++ Calculadora/src/calculadora.java
@@ -0,0 +1,28 @@
+class calculadora {
+	//Primero los atributos
+	float num1;
+	float num2;
+	//Se hace una llamada a los constructores
+	public calculadora(){
+		num1=0;
+		num2=0;
+	}
+	public calculadora(float numero1,float numero2){
+		this.num1=numero1;
+		this.num2=numero2;
+	}
+	//Se crean las operaciones (METODOS)
+	public float MULTIPLICACION(){
+		return num1*num2;
+	}
+	public float DIVISION(){
+		return num1/num2;
+	}
+	public float SUMA(){
+		return num1+num2;
+	}
+	public float RESTA(){
+		return num1-num2;
+	}
+}
+
